25b701fd2f7731f96ca2da0f7d8e7c6a0e62fd1a
KiranNinave <ninavekirankn@gmail.com>
2019-08-28 15:45:27 +0530

    brightcove player added!

--- client/wowza/src/App.js
+++ client/wowza/src/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
+import ReactPlayer from "react-player";
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: null
+        };
+    }
+    componentDidMount() {
+        this.fetchVideo();
+    }
+
+    fetchVideo = async () => {
+        try {
+            const response = await fetch("http://192.168.0.139:4000", {});
+            const json = await response.json();
+            console.log(json);
+            this.setState({
+                data: json
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    };
     render() {
         return (
             <div>
-                <h1>wowza client</h1>
+                {!this.state.data && <h1>loading</h1>}
+                {this.state.data && (
+                    <ReactPlayer url={this.state.data.link} controls={true} />
+                )}
             </div>
         );
     }
